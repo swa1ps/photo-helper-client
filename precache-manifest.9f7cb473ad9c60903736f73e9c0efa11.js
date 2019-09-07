23880c202e8e4621993d1cf74d200be6f0726620
Alexander Ergin <swaips135@gmail.com>
2019-09-07 21:03:54 +0500

    Updates

--- precache-manifest.9f7cb473ad9c60903736f73e9c0efa11.js
+++ precache-manifest.9f7cb473ad9c60903736f73e9c0efa11.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/photo-helper-client/static/js/runtime~main.ae6aed07.js"
   },
   {
-    "revision": "3d2bda2ec982ef76c415",
-    "url": "/photo-helper-client/static/js/main.959c94b3.chunk.js"
+    "revision": "5d64c3d4a4ea0003588a",
+    "url": "/photo-helper-client/static/js/main.6bde9518.chunk.js"
   },
   {
-    "revision": "8b0166a86edb5ee81bdb",
-    "url": "/photo-helper-client/static/js/2.15eaf012.chunk.js"
+    "revision": "85b0d6659c09506638fc",
+    "url": "/photo-helper-client/static/js/2.28517135.chunk.js"
   },
   {
-    "revision": "3d2bda2ec982ef76c415",
+    "revision": "5d64c3d4a4ea0003588a",
     "url": "/photo-helper-client/static/css/main.f26f8fa4.chunk.css"
   },
   {
-    "revision": "8b0166a86edb5ee81bdb",
+    "revision": "85b0d6659c09506638fc",
     "url": "/photo-helper-client/static/css/2.17a92958.chunk.css"
   },
   {
-    "revision": "d2cbaa385b5d329a7c872ecfaeca48e3",
+    "revision": "497c9618ea23aa6cb8158cce1f628998",
     "url": "/photo-helper-client/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/photo-helper-client/precache-manifest.ac2962cc2cac13288159387b6a212770.js"
+  "/photo-helper-client/precache-manifest.9f7cb473ad9c60903736f73e9c0efa11.js"
 );
 
 workbox.clientsClaim();
