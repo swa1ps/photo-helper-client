ac0afb41f5bcf650c450f007eea488e5c08a2430
Alexander Ergin <swaips135@gmail.com>
2019-09-07 17:58:32 +0500

    Updates

---
+++ precache-manifest.c86a6a94f8425edbf01241379b56f349.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/photo-helper-client/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "0e4d6eb202bf95447d27",
+    "url": "/photo-helper-client/static/js/runtime~main.ae6aed07.js"
+  },
+  {
+    "revision": "53a0664951c1aed5faea",
+    "url": "/photo-helper-client/static/js/main.6bcaeffa.chunk.js"
+  },
+  {
+    "revision": "67996d08cc50c1556d3a",
+    "url": "/photo-helper-client/static/js/2.bfc4bf8e.chunk.js"
+  },
+  {
+    "revision": "53a0664951c1aed5faea",
+    "url": "/photo-helper-client/static/css/main.511c9779.chunk.css"
+  },
+  {
+    "revision": "67996d08cc50c1556d3a",
+    "url": "/photo-helper-client/static/css/2.17a92958.chunk.css"
+  },
+  {
+    "revision": "0cf54921005f6f1e3c89fa14df1a30de",
+    "url": "/photo-helper-client/index.html"
+  }
+];
